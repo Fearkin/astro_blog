9da84beb2388ee253c2c910680970586b73b45a9
Fearkin <aloneWorker@yandex.ru>
2023-08-07 23:17:43 +0600

    ending commit

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function get() {
+  return rss({
+    title: 'Astro Learner | Blog',
+    description: 'My journey learning Astro',
+    site: 'https://steady-douhua-80ae1e.netlify.app',
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-us</language>`,
+  });
+}
